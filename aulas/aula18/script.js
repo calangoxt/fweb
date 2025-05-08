a089a8215be695c7cabe5f1977a2750a543a2444
Miguel Donato <migueldonato808@gmail.com>
2025-05-08 20:18:37 -0300

    fim

--- aulas/aula18/script.js
+++ aulas/aula18/script.js
@@ -17,71 +17,70 @@ let btnProximo = document.querySelector("#btnProximo");
 
 let numeroPokedex = 1;
 
-const fetchPokemon = async(pokemon)=>{
-    const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIresponse.json();
-    return data
+const fetchPokemon = async (pokemon) => {
+  const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  const data = await APIresponse.json();
+  return data
 }
-const showPokemon = async(pokemon) =>{
-    const dataPokemon = await fetchPokemon(pokemon);
-    const audio = new Audio(dataPokemon.cries.latest);
-    audio.play();
- 
-try {
-    // Tenta definir a imagem padrão
-    if (dataPokemon.sprites.front_default) {
-      imgPK.src = dataPokemon.sprites.front_default;
-    } else if (dataPokemon.sprites.front_shiny) {
-      // Se não existir, tenta a shiny
-      imgPK.src = dataPokemon.sprites.front_shiny;
-    } else {
-      // Se nenhuma imagem existir, usa fallback
-      imgPK.src = 'img/nao-encontrado.png';
-    }
-  
-    // Caso a imagem falhe ao carregar (ex: URL quebrada)
-    imgPK.onerror = () => {
-      imgPK.src = 'img/nao-encontrado.png';
-    };
-  
-  } catch (erro) {
-    console.error('Erro ao definir imagem do Pokémon:', erro);
-    // Fallback total se algo der muito errado
-    if (imgPK) {
-      imgPK.src = 'img/nao-encontrado.png';
-    }
+const showPokemon = async (pokemon) => {
+  const dataPokemon = await fetchPokemon(pokemon);
+  const audio = new Audio(dataPokemon.cries.latest);
+  audio.play();
+shiny(dataPokemon)
+  idPK.innerHTML = `<br>ID:${dataPokemon.id}`;
+  nomePK.innerHTML = dataPokemon.name;
+  tipo1.innerHTML = ` type:${dataPokemon.types[0].type.name}`;
+  if (
+    dataPokemon.types[1] != undefined
+  ) {
+    tipo2.innerHTML = dataPokemon.types[1].type.name;
   }
-    idPK.innerHTML = `<br>ID:${dataPokemon.id}`;
-    nomePK.innerHTML = dataPokemon.name;
-    tipo1.innerHTML = dataPokemon.types[0].type.name;
-    if(
-        dataPokemon.types[1] != undefined
-    ){
-    tipo2.innerHTML = dataPokemon.types[1].type.name;}
-    else{
-        tipo2.innerHTML = ""
-    }
-    habiPK.innerHTML = dataPokemon.abilities[0].ability.name;
-    pesoPK.innerHTML =`Peso:${dataPokemon.weight/10}kg`;
-    alturaPK.innerHTML =`Altura:${dataPokemon.height/10}m`;
+  else {
+    tipo2.innerHTML = ""
+  }
+  habiPK.innerHTML = dataPokemon.abilities[0].ability.name;
+  pesoPK.innerHTML = `Peso:${dataPokemon.weight / 10}kg`;
+  alturaPK.innerHTML = `Altura:${dataPokemon.height / 10}m`;
 }
-formPK.addEventListener("submit",(event)=>{
-    event.preventDefault();
-    showPokemon(inputPK.value.toLowerCase());
+formPK.addEventListener("submit", (event) => {
+  event.preventDefault();
+  showPokemon(inputPK.value.toLowerCase());
 })
-btnVoltar.addEventListener("click",(e)=>{
-    if(numeroPokedex>1){
-        numeroPokedex--;
-    }
-    showPokemon(numeroPokedex);
+btnVoltar.addEventListener("click", (e) => {
+  if (numeroPokedex > 1) {
+    numeroPokedex--;
+  }
+  showPokemon(numeroPokedex);
 })
-btnProximo.addEventListener("click",(e)=>
-{
-if(numeroPokedex<1025){
+btnProximo.addEventListener("click", (e) => {
+  if (numeroPokedex < 1025) {
     numeroPokedex++;
-}
-showPokemon(numeroPokedex);
+  }
+  showPokemon(numeroPokedex);
 })
+function shiny(dataPokemon){
+  let shiny= Math.floor(Math.random() * 10)
+  if(shiny==10){
+    imgPK.src = dataPokemon.sprites.front_shiny;
+  }else{
+    imgPK.src = dataPokemon.sprites.front_default;
+  }
+}
+let musicaFundo = true;
 
+function musicSistema() {
+    const music = document.getElementById('background-music');
+    const musicIcon = document.getElementById('music-icon');
 
+    // -- Altera entre icones quando clicar para parar e retomar. -->
+    if (musicaFundo) {
+        music.pause();
+        musicaFundo = false;
+        musicIcon.src = './img/sem-som.png';
+    } else {
+        music.play();
+        musicaFundo = true;
+        musicIcon.src = 'img/alto-falante.png'; 
+    }
+}
 showPokemon(numeroPokedex);
